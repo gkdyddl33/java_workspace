af2d4b3c9116788a4fb17541e93c50dda72300a0
gkdyddl33 <70255365+gkdyddl33@users.noreply.github.com>
2020-10-22 15:40:49 +0900

    commit

--- project1019/DataType4.java
+++
@@ -1,8 +0,0 @@
-class DataType4{
-	public static void main(String[] args){
-		short s = 7;
-		byte b = 5;
-		s=b // 가능
-		b=(byte)s // 강제형변환 큰->작
-	}
-}
--- project1019/Test2.java
+++
@@ -1,8 +0,0 @@
-
-class Test2{
-	int x = 7;
-
-	public static void main(String[] args){
-		// x값을 10으로 변경하고 출력하시오.
-	}
-}
