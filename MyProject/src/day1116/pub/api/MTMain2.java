f485c15ef3daea8573a902627e4274b64fbb6b85
gkdyddl33 <70255365+gkdyddl33@users.noreply.github.com>
2020-11-16 16:04:54 +0900

    [UPDATE] 전국의 산 정보 나오게 코딩 내용 수정

--- MyProject/src/day1116/pub/api/MTMain2.java
+++ MyProject/src/day1116/pub/api/MTMain2.java
@@ -3,7 +3,16 @@ package day1116.pub.api;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,6 +20,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
 
 public class MTMain2 extends JFrame{
 
@@ -34,6 +48,18 @@ public class MTMain2 extends JFrame{
 	여전히 2차원 배열의 구조를 유지하므로 TableModel을 이용한 벡터 및 VO를 종합해서 개발해보자.
 */	
 	MountainModel mountainModel;
+	
+	SAXParserFactory factory;
+	SAXParser saxParser;
+	String apiKey = "9aN6wUTG6RTz3a7xwiq8db%2F1ivVh8M%2FVCHzbTD%2FYfNWd2Q41DpXMUwqrAvk5DdPcp4qhcLeohymnypaSEKsrIg%3D%3D";
+	Thread loadThread;	// 네트워크 상에서 xml을 불러올 때 사용할 쓰레드
+									// 버튼을 누를 때마다 인스턴스 생성하여 사용하자.
+	
+	InputStream is;	// xml 데이터를 담고 있는 스트림	
+	MountainHandler mountainHandler;
+	
+	BufferedReader rd;		// 다 읽고 나서 파싱이 끝나면 stream을 닫아야 하므로 변수로 선언함
+	HttpURLConnection conn;
 	
 	public MTMain2() {
 		p_west = new JPanel();
@@ -63,12 +89,110 @@ public class MTMain2 extends JFrame{
 		t_op2.setPreferredSize(new Dimension(190,30));
 		t_op3.setPreferredSize(new Dimension(190,30));
 		
+		// 1) 파싱 시작 버튼과 리스너 연결
+		bt.addActionListener((e)->{
+			// 네트워크를 타고 데이터를 가져올 때 메인 쓰레드에서 진행하는게 좋다			
+			//loadXML();
+			// 쓰레드를 여러번 사용하기 위해서 버튼을 누를 때마다..그럴 경우 이렇게 코드를 
+			// 같이 사용하게 넣어주자. = 무한대
+			loadThread = new Thread() {
+				public void run() {
+					loadXML(); // 버튼을 누를 때 이 쓰레드 호출!!!!!!
+				}
+			};
+			loadThread.start(); // xml 로딩 쓰레드 호출!
+		});
+		
 		setSize(900,600);
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	public void loadXML() {
+		// 1) 공공데이터포털 정보 삽입 - 키 값은 멤버변수에 선언!
+		// URL에서 XML을 가져오는 단계이므로 메서드명을 살짝 바꾸자. 아직 파싱 작업 아님
+		try {
+			StringBuilder urlBuilder = new StringBuilder("http://openapi.forest.go.kr/openapi/service/trailInfoService/getforeststoryservice"); /*URL*/
+			urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "="+apiKey); /*Service Key*/
+			urlBuilder.append("&" + URLEncoder.encode("mntnNm","UTF-8") + "=" + URLEncoder.encode(t_name.getText(), "UTF-8")); /*산이름*/
+			urlBuilder.append("&" + URLEncoder.encode("mntnHght","UTF-8") + "=" + URLEncoder.encode(t_op1.getText(), "UTF-8")); /*산높이*/
+			urlBuilder.append("&" + URLEncoder.encode("mntnAdd","UTF-8") + "=" + URLEncoder.encode(t_op2.getText(), "UTF-8")); /*산소재지*/
+			urlBuilder.append("&" + URLEncoder.encode("mntnInfoAraCd","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /**/
+			urlBuilder.append("&" + URLEncoder.encode("mntnInfoSsnCd","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /**/
+			urlBuilder.append("&" + URLEncoder.encode("mntnInfoThmCd","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /**/
+			URL url = new URL(urlBuilder.toString());
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+			System.out.println("Response code: " + conn.getResponseCode());
+			
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			    rd = new BufferedReader(new InputStreamReader(is=conn.getInputStream()));
+			} else {
+			    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+			// 아래의 코드는 화면에 출력하기 위한 코드이므로, 테스트가 끝나면 제거해야 한다.
+			// 이유? read를 여기서 해버리면 이후의 라인에서는 스트림의 끝에 도달하게 되므로,
+			// 즉 사용전에 써버렸으므로.. 파싱할때 읽어와야 하는데 그 전에 사용했다는 뜻!
+			// 파싱도 하기 전에 소모시키지 말자!
+/*			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = rd.readLine()) != null) {
+			    sb.append(line);
+			}
+*/			
+			// 제대로 불러와 지는지 sb를 콘솔에 출력해보기 -> inputstream으로 실제 출력 확인
+			//System.out.println(sb.toString());
+			
+			parseData(); // xml이 다 읽어들인 시점에 파싱을 시작하자!!			
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	// 2)
+	public void parseData() {
+		/*
+		 	지금 파싱할 데이터는 주의!
+		 	1) xml 파일로 존재할까요?
+		 	2) 메모리 상에서 텍스트로 존재할까요?
+		 	저번주 예제에서는 xml 파일로 보유한 후 파싱했지만, 공공데이터 포털의 api는 메모리상에서
+		 	불러와서 처리해야 해요.. 그래서 parsing 할 때 오버로딩 메서드를 잘 선택해야 한다.
+		*/
+		factory = SAXParserFactory.newInstance();
+		try {
+			saxParser = factory.newSAXParser();	// 파서객체 생성
+			saxParser.parse(is, mountainHandler = new MountainHandler());
+			
+			// 3) MountainHandler 완성후 파싱이 끝나면 TableModel의 벡터객체를 파싱한 결과로
+			// 얻은 벡터로 대체해버리면 된다..
+			mountainModel.data = mountainHandler.mtList;
+			table.updateUI();
+			
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(rd!=null) {
+				try {				
+					rd.close();					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}				
+			}
+			if(conn != null)conn.disconnect();
+		}
+	}
 	public static void main(String[] args) {
 		new MTMain2();
 	}
--- MyProject/src/day1116/pub/api/MountainHandler.java
+++ MyProject/src/day1116/pub/api/MountainHandler.java
@@ -1,35 +1,92 @@
 package day1116.pub.api;
 
+import java.util.Vector;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 /*
  	SAX Parsing 시 모든 node(요소, 텍스트 등 xml 을 이루는 요소들을 일컬음) 마다
  	이벤트를 발생시켜주는 객체
+ 	공공데이터포털의 실제 데이터를 가져오기 위해서는 Parsing을 해야 함..
 */
 public class MountainHandler extends DefaultHandler{
+	// 발견되는 산이 있을 때 VO로 생성후, 그 VO를 담게 될 벡터
+	Vector<Mountain> mtList;
+	Mountain mt;	// 산의 정보 1건을 임시적으로 담을 변수
+	
+	// 현재 실행부가 어느 위치를 지났는지를 알기 위한 변수
+	boolean isMntnid;
+	boolean isMntnnm;
+	boolean isMntninfopoflc;
+	boolean isMntninfohght;
 	
 	public void startDocument() throws SAXException {
-		
+		// 문서가 시작되면 벡터를 생성한다.		
 	}
 	
 	// 시작태그
 	public void startElement(String uri, String localName, String tag, Attributes attributes) throws SAXException {
-			
+		System.out.println("<"+tag+">");
+		
+		if(tag.equals("items")) {// 여기서 벡터 생성
+			mtList = new Vector<Mountain>();
+		}else if(tag.equals("item")) {// 여기서 VO 생성
+			mt = new Mountain();
+		}else if(tag.equals("mntnid")) {// 실행부가 지나간다.
+			isMntnid=true;
+		}else if(tag.equals("mntnnm")) {
+			isMntnnm=true;
+		}else if(tag.equals("mntninfopoflc")) {
+			isMntninfopoflc=true;
+		}else if(tag.equals("mntninfohght")) {
+			isMntninfohght=true;
+		}
 	}
 	
 	// 중간태그
 	public void characters(char[] ch, int start, int length) throws SAXException {
-			
+		String data = new String(ch, start, length);
+		System.out.println(data);
+		
+		//태그위치에 따른 데이터 담기 
+		if(isMntnid) {
+			mt.setMntnid(Integer.parseInt(data)); //산의 아이디 담기
+		}else if(isMntnnm) {
+			mt.setMntnnm(data);//산의 이름
+		}else if(isMntninfopoflc) {
+			mt.setMntninfopoflc(data);//산의 위치
+		}else if(isMntninfohght) {
+			mt.setMntninfohght(Integer.parseInt(data)); //산의높이
+		}
 	}
 	
 	// 닫는태그
 	public void endElement(String uri, String localName, String tag) throws SAXException {
-			
-	}
-	
+		System.out.println("</"+tag+">");	
+		if(tag.equals("mntnid")) {// 실행부가 지나간다.
+			isMntnid=false;
+		}else if(tag.equals("mntnnm")) {
+			isMntnnm=false;
+		}else if(tag.equals("mntninfopoflc")) {
+			isMntninfopoflc=false;
+		}else if(tag.equals("mntninfohght")) {
+			isMntninfohght=false;
+		}else if(tag.equals("item")) {
+			mtList.add(mt);			
+		}
+	}	
 	
 	public void endDocument() throws SAXException {
-			
+		System.out.println("검색된 산의 갯수는 "+mtList.size());
+		
+		for(int i=0;i<mtList.size();i++) {
+			Mountain obj = mtList.get(i);
+			System.out.println("산ID"+obj.getMntnid());
+			System.out.println("산이름"+obj.getMntnnm());
+			System.out.println("산위치"+obj.getMntninfopoflc());
+			System.out.println("산높이"+obj.getMntninfohght());
+			System.out.println("------------------------------------------------");
+		}
 	}
 }
